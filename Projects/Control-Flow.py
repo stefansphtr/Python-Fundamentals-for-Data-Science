694035f1ce0922521364a1d6e19cbdee36750379
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-01-15 11:15:22 +0700

    chore(medical-insurance-project): add keanu and my estimated insurance cost

--- Projects/Control-Flow.py
+++ Projects/Control-Flow.py
@@ -29,3 +29,9 @@ def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
     estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
     print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
     return estimated_cost
+
+# Estimate Keanu's insurance cost
+keanu_insurance_cost = estimate_insurance_cost(name = 'Keanu', age = 29, sex = 1, num_of_children = 3, smoker = 1)
+
+# My estimate insurance cost
+my_insurance_cost = estimate_insurance_cost(name="Stefan", age=21, smoker=1, sex=1, num_of_children=0)
