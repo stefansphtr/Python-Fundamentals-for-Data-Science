d16eab42a758d83bb18ceb48e9a8af4b0a4fae87
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-01-15 11:10:06 +0700

    docs(medical-insurance-project): add docstring to function estimate_insurance_cost

--- Projects/Control-Flow.py
+++ Projects/Control-Flow.py
@@ -4,6 +4,19 @@ def analyze_smoker(smoker_status):
 
 # Function to estimate insurance cost:
 def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
-  estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
-  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
-  return estimated_cost
+    """
+    Calculates the estimated insurance cost based on the given parameters.
+
+    Parameters:
+    - name (str): The name of the person.
+    - age (int): The age of the person.
+    - sex (int): The sex of the person (0 for female, 1 for male).
+    - num_of_children (int): The number of children the person has.
+    - smoker (int): Indicates whether the person is a smoker (0 for non-smoker, 1 for smoker).
+
+    Returns:
+    - estimated_cost (float): The estimated insurance cost in dollars.
+    """
+    estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
+    print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+    return estimated_cost
