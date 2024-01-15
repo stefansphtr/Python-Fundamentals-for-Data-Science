a8379ce5dde5f2aa28739a2dc0b01971ace3c31e
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-01-15 11:08:15 +0700

    chore(medical-insurance-project): complete create function to analyze smoker status

--- Projects/Control-Flow.py
+++ Projects/Control-Flow.py
@@ -1,3 +1,7 @@
+def analyze_smoker(smoker_status):
+  if smoker_status == 1:
+    print("To lower your cost, you should consider quitting smoking.")
+
 # Function to estimate insurance cost:
 def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
   estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
