f2f3011afbd265779d72c32e55a7d630b8142d5a
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-01-15 11:20:01 +0700

    chore(medical-insurance-project): add analyze_smoker function inside estimate_insurance_cost to test out the function

--- Projects/Control-Flow.py
+++ Projects/Control-Flow.py
@@ -30,6 +30,7 @@ def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
     """
     estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
     print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+    analyze_smoker(smoker)
     return estimated_cost
 
 # Estimate Keanu's insurance cost
