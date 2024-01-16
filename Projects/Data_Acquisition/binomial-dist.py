ae738ca1c22b2abe78c03c5f9fcde42b44f94f66
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-01-16 23:13:36 +0700

    chore(data-acquisition): create binomial distribution with numpy

---
+++ Projects/Data_Acquisition/binomial-dist.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# coin_binomial = np.random.binomial(n=1, p=0.8, size=500)
+
+# print(coin_binomial)
+
+coin_binomial = np.random.binomial(n=100, p=0.8, size=500) 
+
+print(coin_binomial)
