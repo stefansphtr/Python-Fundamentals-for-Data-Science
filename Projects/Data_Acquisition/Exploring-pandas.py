70874ef1e0b7df4fe0b3d156c6481e3394e86530
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-01-16 22:55:50 +0700

    style(data-acquisition): improve code readability by add comment in Exploring-pandas.py

--- Projects/Data_Acquisition/Exploring-pandas.py
+++ Projects/Data_Acquisition/Exploring-pandas.py
@@ -1,12 +1,21 @@
+# Import the pandas library
 import pandas as pd
 
+# Read the CSV file into a DataFrame
 commute_df = pd.read_csv("./Projects/Data_Acquisition/Data/commute_data.csv")
 
+# Print the first 5 rows of the DataFrame
 print(commute_df.head())
 
+"""
+Rename the columns of the DataFrame: 
+- "name" for the name of the county
+- "total_commuters" for the total number of commuters (B08303_001E)
+- "long_commute_90min_plus" for the number of commuters who travel 90 or more minutes (B08303_013E)
+- "state" for the state code
+- "county" for the county code
+"""
 commute_df.columns = ["name", "total_commuters", "long_commute_90min_plus", "state", "county"]
-# total_commuters for "Total commuters count" (B08303_001E)
-# long_commute_90min_plus for "The count for commuters who travel 90 or more minutes" (B08303_013E)
 
+# Print the first 5 rows of the DataFrame after renaming the columns
 print(commute_df.head())
-
